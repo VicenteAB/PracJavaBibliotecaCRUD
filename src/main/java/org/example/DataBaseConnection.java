26611d33a75371931b1072287ccbf65c84dce88e
Vicente Aburto <v.aburto00@gmail.com>
2026-06-24 00:46:30 -0400

    CRUD biblioteca con PreparedStatement y ResultSet enviar datos a una BD

--- src/main/java/org/example/DataBaseConnection.java
+++ src/main/java/org/example/DataBaseConnection.java
@@ -3,24 +3,28 @@ package org.example;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DataBaseConnection {
 
-    // Configuración para base de datos
-    private static final String URL = "jdbc:mysql://localhost:3306/biblioteca?useSSL=false&serverTimezone=UTC";
-    private static final String USUARIO = "root";
-    private static final String CONTRASENA = "Gt-m3710";
-
-    private static Connection instancia = null;
-
-
     public static Connection conectar() {
+
         Connection conexion = null;
+
         try {
+            Properties props = new Properties();
+            props.load(DataBaseConnection.class.getClassLoader().getResourceAsStream("config.properties"));
+
+            // Configuración para base de datos
+            String URL = props.getProperty("db.url");
+            String USUARIO = props.getProperty("db.usuario");
+            String CONTRASENA = props.getProperty("db.contrasena");
+
             // Cargar el driver y establecer la conexión
             conexion = DriverManager.getConnection(URL, USUARIO, CONTRASENA);
             System.out.println("¡Conexión exitosa a la base de datos!");
-        } catch (SQLException e) {
+
+        } catch (Exception e) {
             System.out.println("Error de conexión: " + e.getMessage());
         }
         return conexion;
